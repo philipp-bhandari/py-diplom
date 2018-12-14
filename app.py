69d4c1754464c5e8525acc3397def7df521119e7
feelsever <philipp.bhandari@gmail.com>
2018-12-14 22:13:24 +0300

    diplom start

---
+++ app.py
@@ -0,0 +1,20 @@
+from urllib.parse import urlencode
+import requests
+from pprint import pprint
+import time
+
+
+APP_ID = 6784110
+USER_ID = '171691064'
+
+
+
+class User:
+    pass
+
+class SpyGame:
+    pass
+
+
+
+
